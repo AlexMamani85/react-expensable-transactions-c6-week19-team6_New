d3f44801f1a56b9ac513f3f6310cf1dda03536e5
Alex Mamani <alex.mamani@tecsup.edu.pe>
2022-07-03 20:49:56 -0500

    feat: colorText in cards for transactionsPage

--- src/components/CategoryCard/category-card.js
+++ src/components/CategoryCard/category-card.js
@@ -2,7 +2,7 @@ import CircleIcon from "../CircleIcon";
 import * as Styled from "./styles";
 import PropTypes from "prop-types";
 
-function CategoryCard({ id, name, color, Icon, amount, onCategoryClick }) {
+function CategoryCard({ id, name, color, colorText, Icon, amount, onCategoryClick }) {
   return (
     <Styled.Wrapper
       color={color}
@@ -11,7 +11,7 @@ function CategoryCard({ id, name, color, Icon, amount, onCategoryClick }) {
       <CircleIcon Icon={Icon} size="lg" color={color} />
       <div>
         <Styled.CategoryName>{name}</Styled.CategoryName>
-        <Styled.CategoryAmount>
+        <Styled.CategoryAmount  color={colorText}>
           <span>$</span>
           <span>{Intl.NumberFormat("en-US").format(amount)}</span>
         </Styled.CategoryAmount>
