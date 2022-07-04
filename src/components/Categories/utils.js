03b80e40e8855122770e7f3ccc89376dd373b5e0
Alex Mamani <alex.mamani@tecsup.edu.pe>
2022-07-03 20:50:17 -0500

    feat: getMonthlyDataForTransactions

--- src/components/Categories/utils.js
+++ src/components/Categories/utils.js
@@ -23,3 +23,36 @@ export const getMonthlyData = (categories, date, type) => {
       };
     });
 };
+
+export const getMonthlyDataForTransactions = (categories, date) => {
+
+
+  // TODO {id: 449, name: 'salary', transaction_type: 'income', color: 'red', icon: 'bank', …}
+  // ! ASI TERMINA: {id: 449, name: 'salary', color: '#EF4444', amount: 3000, Icon: ƒ}
+
+  //  { transaction_type: 'income', color: 'red', icon: 'bank', …}
+  //  {  color: '#EF4444', amount: 3000, Icon: ƒ}
+
+  return categories
+    .map((cat) => {
+      const colorText = cat['transaction_type'] === "expense" ? "red" : "green"
+      return {
+        id: cat.id,
+        name: cat.name,
+        Icon: categoryIcons[cat.icon],
+        color: categoryColors[cat.color],
+        colorText: colorText,
+        transaction_type: cat['transaction_type'],
+        amount: cat.transactions.reduce((acc, cur) => {
+          const trxDate = parseISO(cur.date);
+          const trxYear = trxDate.getFullYear();
+          const trxMonth = trxDate.getMonth();
+          if (trxYear === date.year && trxMonth === date.month) {
+            return acc + cur.amount;
+          } else {
+            return acc;
+          }
+        }, 0),
+      };
+    });
+};
